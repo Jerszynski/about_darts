ab5f39fe13054e54c82b274f1dde88f8421cbbcb
Jerszynski <krzysztof.jerszynski@o2.pl>
2023-03-05 23:17:14 +0100

    Add navigation.css and styling. Remove button. Clear script.js

--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-console.log("Cześć");
-
-let header = document.querySelector(".header");
-let button = document.querySelector(".js-button");
-
-button.addEventListener("click", () => {
-  header.remove();
-});
