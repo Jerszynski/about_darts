5f0e73d749c8e77f9844af7d2423d97acc3dd3f3
Jerszynski <krzysztof.jerszynski@o2.pl>
2023-03-07 01:26:38 +0100

    Add functionality to both buttons on the page

---
+++ javascript/script.js
@@ -0,0 +1,36 @@
+const tableElement = document.querySelector(".js-table");
+const buttonRemoveElement = document.querySelector(
+  ".js-buttonTableVisibilityToggle"
+);
+const darkModeButtonElement = document.querySelector(".js-darkModeButton");
+const mainContainerElement = document.querySelector(".js-mainContainer");
+const navigationLinkElement = document.querySelectorAll(".js-navigationLink");
+const bodyElement = document.querySelector(".body");
+const tableRowElement = document.querySelectorAll(".js-tableRow");
+const tableCellHeaderElement = document.querySelectorAll(".js-tableHeader");
+
+buttonRemoveElement.addEventListener("click", () => {
+  buttonRemoveElement.innerText = tableElement.classList.toggle("table--hidden")
+    ? "Pokaż tabelę"
+    : "Ukryj Tabelę";
+});
+
+darkModeButtonElement.addEventListener("click", () => {
+  mainContainerElement.classList.toggle("mainContainer--darkMode");
+  mainContainerElement.classList.toggle("mainContainer--darkModeShadow");
+  bodyElement.classList.toggle("mainContainer--darkMode");
+  darkModeButtonElement.classList.toggle("darkModeButton");
+  buttonRemoveElement.classList.toggle("darkModeButton");
+
+  navigationLinkElement.forEach((link) => {
+    link.classList.toggle("mainContainer--darkMode");
+  });
+
+  tableRowElement.forEach((row) => {
+    row.classList.toggle("table__row--dark");
+  });
+
+  tableCellHeaderElement.forEach((header) => {
+    header.classList.toggle("table__cell--headerDark");
+  });
+});
