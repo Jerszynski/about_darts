da9fd25b587eff630b8ef851fd747782da9dd16f
Jerszynski <krzysztof.jerszynski@o2.pl>
2023-03-18 22:32:10 +0100

    Refactor script.js file

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,63 @@
-const tableElement = document.querySelector(".js-table");
-const buttonRemoveElement = document.querySelector(
-  ".js-buttonTableVisibilityToggle"
-);
-const darkModeButtonElement = document.querySelector(".js-darkModeButton");
-const mainContainerElement = document.querySelector(".js-mainContainer");
-const navigationLinkElement = document.querySelectorAll(".js-navigationLink");
-const bodyElement = document.querySelector(".body");
-const tableRowElement = document.querySelectorAll(".js-tableRow");
-const tableCellHeaderElement = document.querySelectorAll(".js-tableHeader");
+{
+  const buttonRemoveElement = document.querySelector(
+    ".js-buttonTableVisibilityToggle"
+  );
 
-buttonRemoveElement.addEventListener("click", () => {
-  buttonRemoveElement.innerText = tableElement.classList.toggle("table--hidden")
-    ? "Pokaż tabelę"
-    : "Ukryj Tabelę";
-});
+  const onButtonRemoveClick = () => {
+    const tableElement = document.querySelector(".js-table");
 
-darkModeButtonElement.addEventListener("click", () => {
-  mainContainerElement.classList.toggle("mainContainer--darkMode");
-  bodyElement.classList.toggle("body--darkMode");
-  darkModeButtonElement.classList.toggle("darkModeButton");
-  buttonRemoveElement.classList.toggle("darkModeButton");
+    buttonRemoveElement.innerText = tableElement.classList.toggle(
+      "table--hidden"
+    )
+      ? "Pokaż tabelę"
+      : "Ukryj Tabelę";
+  };
 
-  navigationLinkElement.forEach((link) => {
-    link.classList.toggle("navigation__link--darkMode");
-  });
+  const navigationLinkDarkMode = () => {
+    const navigationLinkElement =
+      document.querySelectorAll(".js-navigationLink");
 
-  tableRowElement.forEach((row) => {
-    row.classList.toggle("table__row--dark");
-  });
+    navigationLinkElement.forEach((link) => {
+      link.classList.toggle("navigation__link--darkMode");
+    });
+  };
 
-  tableCellHeaderElement.forEach((header) => {
-    header.classList.toggle("table__cell--headerDark");
-  });
-});
+  const tableRowDarkMode = () => {
+    const tableRowElement = document.querySelectorAll(".js-tableRow");
+
+    tableRowElement.forEach((row) => {
+      row.classList.toggle("table__row--dark");
+    });
+  };
+
+  const tableCellDarkMode = () => {
+    const tableCellHeaderElement = document.querySelectorAll(".js-tableHeader");
+
+    tableCellHeaderElement.forEach((header) => {
+      header.classList.toggle("table__cell--headerDark");
+    });
+  };
+
+  const darkModeButtonElement = document.querySelector(".js-darkModeButton");
+  const onDarkModeButtonClick = () => {
+    const mainContainerElement = document.querySelector(".js-mainContainer");
+    const bodyElement = document.querySelector(".body");
+
+    mainContainerElement.classList.toggle("mainContainer--darkMode");
+    bodyElement.classList.toggle("body--darkMode");
+    darkModeButtonElement.classList.toggle("darkModeButton");
+    buttonRemoveElement.classList.toggle("darkModeButton");
+
+    navigationLinkDarkMode();
+    tableRowDarkMode();
+    tableCellDarkMode();
+  };
+
+  const init = () => {
+    darkModeButtonElement.addEventListener("click", onDarkModeButtonClick);
+
+    buttonRemoveElement.addEventListener("click", onButtonRemoveClick);
+  };
+
+  init();
+}
